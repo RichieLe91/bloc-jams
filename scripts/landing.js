dce28d4c4e53152ab1fa76e72735e9de8c76bfe4
Richie Nguyen Le <richiele91@gmail.com>
2017-03-22 10:03:29 -0700

    Complete checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,26 @@
-var animatePoints = function(index) {
-                
-    var points = document.getElementsByClassName("point");
-    var revealPoints = function(index) {
-        for(var i = 0; i <= index; i++) {      
-          points[i].style.opacity = 1;
-          points[i].style.transform = "scaleX(1) translateY(0)";
-          points[i].style.msTransform = "scaleX(1) translateY(0)";
-          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
-    };
-   revealPoints(index); 
-};
+var pointsArray = document.getElementsByClassName("point");
 
+var animatePoints = function(points) {
+        for(var i = 0; i <= pointsArray.length; i++) {      
+          pointsArray[i].style.opacity = 1;
+          pointsArray[i].style.transform = "scaleX(1) translateY(0)";
+          pointsArray[i].style.msTransform = "scaleX(1) translateY(0)";
+          pointsArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }; 
+}
+                
+   
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName("selling-points")[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+      
+    window.addEventListener('scroll', function(event){
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          animatePoints(pointsArray);     
+      }
+    });
+}
